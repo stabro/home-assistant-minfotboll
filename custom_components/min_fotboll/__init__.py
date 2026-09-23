42fa4450e8b73bc7530a00f54f9272a3e4bd0e20
stabro <77299850+stabro@users.noreply.github.com>
2026-09-23 15:14:07 +0200

    Use options flow automatic reload

--- custom_components/min_fotboll/__init__.py
+++ custom_components/min_fotboll/__init__.py
@@ -52,13 +52,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
-
-
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
