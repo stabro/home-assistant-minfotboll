0b018e52cff29ef1d8536e4b33744bd063ce0239
stabro <77299850+stabro@users.noreply.github.com>
2026-09-23 11:57:43 +0200

    Initial Min Fotboll v0.1.0

---
+++ custom_components/min_fotboll/const.py
@@ -0,0 +1,25 @@
+"""Constants for the Min Fotboll integration."""
+
+from datetime import timedelta
+
+DOMAIN = "min_fotboll"
+NAME = "Min Fotboll"
+VERSION = "0.1.0"
+
+API_BASE_URL = "https://minfotboll-api.azurewebsites.net"
+PLATFORM_ID = "2"
+UPDATE_INTERVAL = timedelta(seconds=90)
+
+CONF_ACCESS_TOKEN = "access_token"
+CONF_REFRESH_TOKEN = "refresh_token"
+CONF_EXPIRES = "expires"
+CONF_SERVER_TIME = "server_time"
+CONF_TOKEN_JSON = "token_json"
+
+STATUS_UPCOMING = "KOMMANDE"
+STATUS_LIVE = "LIVE"
+STATUS_FINISHED = "SLUT"
+STATUS_CANCELLED = "INSTÄLLD"
+STATUS_POSTPONED = "UPPSKJUTEN"
+STATUS_INTERRUPTED = "AVBRUTEN"
+STATUS_UNKNOWN = "OKÄND"
